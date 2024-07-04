df1beb9ee263b5c6472aacaabe6ab4afbc37effa
Shehroz Khan <163621819+shehroz1404@users.noreply.github.com>
2024-07-04 17:43:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+gsap.from(".logo h1",{
+    y:-100,
+    delay:0.20,
+    duration:0.70,
+    stagger:0.30
+})
+gsap.from(".nav li", {
+    y:-100,
+    delay:0.60,
+    duration:0.50,
+    stagger:0.30
+})
+gsap.from(".icons i", {
+    y:-100,
+    delay:0.60,
+    duartion:0.50,
+    stagger:0.30
+})
+gsap.from(".sectionimage img",{
+   opacity:0,
+   scale:0
+})
+gsap.from(".sectiontext h1",{
+    y:100,
+    duration:0.60,
+    delay:0.10
+})
+gsap.from(".sectiontext p",{
+    y:100,
+    duration:0.60,
+    delay:0.10
+})
+gsap.from(".sectiontext a",{
+    opacity:0,
+    scale:0,
+    duration:2
+})
+gsap.from(".sectionimage2 img",{
+    opacity:0,
+    scale:0
+})
+gsap.from(".sectiontext2 h1",{
+    y:100,
+    duration:0.60,
+    delay:0.10
+})
+gsap.from(".sectiontext2 p",{
+    y:100,
+    duration:0.60,
+    delay:0.10
+})
+gsap.from(".sectiontext2 a",{
+    opacity:0,
+    scale:0,
+    duration:2
+})
+
